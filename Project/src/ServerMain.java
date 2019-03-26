764119ae47c1487962aef33ee72a75b6128ec766
UnfoldingCode <46750873+UnfoldingCode@users.noreply.github.com>
2019-03-26 19:18:03 -0400

    Update ServerMain.java

--- Project/src/ServerMain.java
+++ Project/src/ServerMain.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class ServerMain {
 
 	public static void main(String[] args) {
-		
+		//Creates server
 		ServerApp server = new ServerApp();
 		server.setSize(450,250);
 		server.setVisible(true);
